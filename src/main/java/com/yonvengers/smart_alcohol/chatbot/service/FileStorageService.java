6bb00fe25a10acb6f553b95640dc49d52c853e28
aidenyoun <younyw15@gmail.com>
2024-07-03 20:48:52 +0900

    FileStorageService.java
    1.사용자가 파일 업로드 할 때, 파일명에 시각을 KST로 변경.
    2.admin2_20240703_204756_voice1
    계정명_날짜_시각_원본파일명

--- src/main/java/com/yonvengers/smart_alcohol/chatbot/service/FileStorageService.java
+++ src/main/java/com/yonvengers/smart_alcohol/chatbot/service/FileStorageService.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class FileStorageService {
@@ -16,11 +18,17 @@ public class FileStorageService {
     private String uploadDir;
 
     public String storeFile(MultipartFile file, String userId) throws IOException {
+        // 현재 날짜 및 시간 형식 지정
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+        String formattedDateTime = LocalDateTime.now().format(formatter);
+
         // 파일명 생성
-        String filename = userId + "_" + System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        String filename = userId + "_" + formattedDateTime + "_" + file.getOriginalFilename();
+
         // 파일 저장 경로
         Path filePath = Paths.get(uploadDir + java.io.File.separator + filename);
         Files.write(filePath, file.getBytes());
+
         return filePath.toString();
     }
 }
